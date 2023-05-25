8190146bf67d676def7552baf183fb1488e48b1f
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-05-25 13:54:58 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import { useEffect,useState } from "react"
+
+
+function App() {
+const [isLoaded,setIsLoaded]=useState(false)
+const [data,setData]=useState("")
+useEffect(()=>{
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res=>res.json())
+    .then(data=>{
+        setData(data.message)
+    setIsLoaded(true)})
+    if(isLoaded){
+      console.log("loaded")  
+    }else{
+        <p>Loading...</p>
+    }
+},[])
+  return (
+    <>
+    <img src={data} alt="A random dog"/>
+    </>
+  )
+}
+
+export default App
